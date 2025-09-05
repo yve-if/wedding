8e9d6da668a3da3db0de066a402e57a5783901b2
yve-if <ayuu.new1@gmail.com>
2025-09-05 21:54:50 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,18 +2,18 @@ export const data = {
     bride: {
         L: {
             id: 1,
-            name: 'Lorem Ipsum',
-            child: 'Putra ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
+            name: 'Dwi Wasono',
+            child: 'Putra kedua',
+            father: 'Budi Supanto',
+            mother: 'Murtini',
             image: './src/assets/images/cowo.png'
         },
         P: {
             id: 2,
-            name: 'Ipsum Lorem',
-            child: 'Putri ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
+            name: 'Ayu Rohmmana Ainun',
+            child: 'Putri ketiga',
+            father: '(Alm)Slimin',
+            mother: 'Suti Adi Utami',
             image: './src/assets/images/cewe.png'
         },
 
@@ -22,31 +22,31 @@ export const data = {
 
     time: {
         marriage: {
-            year: '2030',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
+            year: '2025',
+            month: 'Juni',
+            date: '18',
+            day: 'Rabu',
             hours: {
                 start: '08.00',
                 finish: 'Selesai'
             }
         },
         reception: {
-            year: '2024',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
+            year: '2025',
+            month: 'September',
+            date: '21',
+            day: 'Minggu',
             hours: {
                 start: '11.00',
                 finish: 'Selesai'
             }
         },
-        address: 'Kp. Lorem, RT 000/ RW 000, Desa.Lorem, Kec.Ipsum, Kab.Lorem, Lorem (1234)'
+        address: 'Jl. Mayjen Sungkono Dusun Dayu Gg. 1, RT 06 / RW 01 Tunggorono, Kec. Jombang'
     },
 
     link: {
-        calendar: 'https://calendar.app.google/oSVLRMYC79GzuA4f9',
-        map: 'https://maps.app.goo.gl/q1Ask2Jgd4ekiiKBA',
+        calendar: 'https://calendar.app.google/P1V8ukBVsvVTGa518',
+        map: 'https://maps.app.goo.gl/Ks3qYTcyGhBCT9HF7',
     },
 
     galeri: [
@@ -75,13 +75,13 @@ export const data = {
     bank: [
         {
             id: 1,
-            name: 'Lorem Ipsum',
+            name: 'Dwi Wasono',
             icon: './src/assets/images/bca.png',
             rekening: '12345678'
         },
         {
             id: 2,
-            name: 'Ipsum Lorem',
+            name: 'Ayu Rohmmana Ainun',
             icon: './src/assets/images/bri.png',
             rekening: '12345678'
         },
@@ -89,7 +89,7 @@ export const data = {
 
     audio: './src/assets/audio/wedding.mp3',
 
-    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+    api: 'https://script.google.com/macros/s/AKfycbz5V1r7ivvutiAkBg50dT4mo1d5gh_eII6euUkSfCmodr_Le484TmmHlgDJbYaJ0uj-/exec',
 
     navbar: [
         {
